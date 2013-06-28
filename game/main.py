086f87424f15796409512f4b1a4bc9704c9ec398
Kalail <kashif610@gmail.com>
2013-06-28 18:01:42 -0500

    recommit

--- game/main.py
+++ game/main.py
@@ -2,23 +2,20 @@ import json
 import logging
 import engine
 from game import TestGame
+from starter import Starter
 # from numba import autojit
 
 # @autojit
 def main():
+    engine_starter = Starter()
+    engine_starter.start()
     # Setup simple logging
-    logging.basicConfig(
-        format='%(levelname)s:%(message)s',
-        level=logging.INFO
-    )
+    # logging.basicConfig(
+    #     format='%(levelname)s:%(message)s',
+    #     level=logging.INFO
+    # )
     # Get config
-    with open('config.json') as config_file:
-        config = json.load(config_file)
     # Start engine
-    game_cls = TestGame
-    eng = engine.Engine(config, game_cls)
-    # Run engine
-    eng.start()
 
 if __name__ == '__main__':
     main()
